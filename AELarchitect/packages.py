8545905711870c182c122871d8177d24ea935087
Pascal Malouin <pascal.malouin@gmail.com>
2023-12-10 07:55:33 -0500

    Updated using lazygit...

--- AELarchitect/packages.py
+++ AELarchitect/packages.py
@@ -3,7 +3,7 @@
 ## [AELarchitect/packages.py]
 ## author        : fantomH @alterEGO Linux
 ## created       : 2023-12-08 11:12:16 UTC
-## updated       : 2023-12-08 11:12:16 UTC
+## updated       : 2023-12-10 02:12:52 UTC
 ## description   : Generates packages lists and stuff.
 
 import sqlite3 as sql
@@ -26,36 +26,60 @@ def packages_list(mode='hacker'):
     con.close
 
 def package_info(package):
+
     pkg_info = execute(f"paru -Si {package}", capture_output=True).stdout.decode("UTF-8").split("\n")
-
-    pkg_Ql = execute(f"paru -Qlq {package}", capture_output=True).stdout.decode("UTF-8").split("\n")
-
-    # pkgs_info = []
-
     package_info = {}
     for line in pkg_info:
+        ## Package name.
         package_info["package"] = package
+        ## Package repository.
         if line.startswith("Repository"):
             package_info["repository"] = line.split(":")[1].strip()
+        ## Package description.
         if line.startswith("Description"):
             package_info["description"] = line.split(":")[1].strip() + "."
+        ## Package URL
         if line.startswith("URL"):
             package_info["url"] = line.split(": ")[1]
 
     if package_info.get('repository') is None:
-        print(f"{package_info.get('package')}")
+        pass
+        # print(f"{package_info.get('package')}")
     else:
         pass
 
-    package_Ql_bin = []
+    print(package_info.get('package'))
+
+    pkg_Ql = execute(f"paru -Qlq {package}", capture_output=True).stdout.decode("UTF-8").split("\n")
+
+    print('++++++++++++++++++++++++++')
     for line in pkg_Ql:
+        print(line)
+        # if line.endswith(".md"):
+            # print(line)
+            # execute(f"glow {line}")
+
+    package_Ql_bin = []
+    package_Ql_desktop = []
+    package_Ql_man = []
+    for line in pkg_Ql:
+        ## Package executables.
         if "/usr/bin/" in line and not line.endswith("/"):
             package_Ql_bin.append(line)
-        else:
-            pass
-            # print(line)
+        ## Package desktop.
+        if line.endswith(".desktop"):
+            package_Ql_desktop.append(line)
+        ## Package man.
+        if "/man/" in line and line.endswith(".gz"):
+            _man = line.split("/")[-1].split(".")
+            _man = f"{'.'.join(_man[0:-2])}({_man[-2]})"
+            package_Ql_man.append(line)
 
     package_info["Ql_bin"] = package_Ql_bin
+    package_info["Ql_desktop"] = package_Ql_desktop
+    package_info["Ql_man"] = package_Ql_man
+
+    # print(package_info)
     
     return ' '
 
