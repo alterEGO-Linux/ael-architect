c88d5ed11b8b14b775e97e458dfe1d3045fdd1bf
Pascal Malouin <pascal.malouin@gmail.com>
2023-12-11 15:07:39 -0500

    Updated using lazygit...

--- AELarchitect/packages.py
+++ AELarchitect/packages.py
@@ -41,6 +41,8 @@ def package_info(package):
         ## Package URL
         if line.startswith("URL"):
             package_info["url"] = line.split(": ")[1]
+        if line.startswith("AUR URL"):
+            package_info["aur_url"] = line.split(": ")[1]
 
     if package_info.get('repository') is None:
         pass
@@ -49,12 +51,12 @@ def package_info(package):
         pass
 
     print(package_info.get('package'))
+    print('++++++++++++++++++++++++++')
+    # print(package_info.get('aur_url'))
 
     pkg_Ql = execute(f"paru -Qlq {package}", capture_output=True).stdout.decode("UTF-8").split("\n")
-
-    print('++++++++++++++++++++++++++')
-    for line in pkg_Ql:
-        print(line)
+    # for line in pkg_Ql:
+        # print(line)
         # if line.endswith(".md"):
             # print(line)
             # execute(f"glow {line}")
@@ -62,6 +64,7 @@ def package_info(package):
     package_Ql_bin = []
     package_Ql_desktop = []
     package_Ql_man = []
+    package_Ql_info = []
     for line in pkg_Ql:
         ## Package executables.
         if "/usr/bin/" in line and not line.endswith("/"):
@@ -73,13 +76,32 @@ def package_info(package):
         if "/man/" in line and line.endswith(".gz"):
             _man = line.split("/")[-1].split(".")
             _man = f"{'.'.join(_man[0:-2])}({_man[-2]})"
-            package_Ql_man.append(line)
+            package_Ql_man.append(_man)
+        ## Info.
+        if "/info/" in line and line.endswith(".gz"):
+            _info = line.split("/")[-1].split(".")
+            _info = f"{'.'.join(_info[0:-2])}"
+            package_Ql_info.append(_info)
+        ## Is Python?
+        if "/usr/lib/python" in line:
+            package_info["is_python"] = True
+            break
+        else:
+            package_info["is_python"] = False
+        ## Documentation.
+        if ("/doc/") in line:
+            package_info["has_docs"] = True
+        else:
+            package_info["has_docs"] = False
 
     package_info["Ql_bin"] = package_Ql_bin
     package_info["Ql_desktop"] = package_Ql_desktop
     package_info["Ql_man"] = package_Ql_man
+    package_info["Ql_info"] = package_Ql_info
 
     # print(package_info)
+    print(package_info.get("Ql_info"))
+    print()
     
     return ' '
 
