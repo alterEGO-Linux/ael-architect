2fbeb8b9a2ed87ad801de7419620fe9995279420
Pascal Malouin <pascal.malouin@gmail.com>
2024-01-19 12:34:29 -0500

    Updated using lazygit...

--- AELarchitect/packages.py
+++ AELarchitect/packages.py
@@ -6,32 +6,44 @@
 ## updated       : 2024-01-16 12:39:23 UTC
 ## description   : Generates packages lists and stuff.
 
+from datetime import (datetime,
+                      timezone)
+import os
 import sqlite3 as sql
+import tomllib
 
 from command import execute
 
 DB = '/home/ghost/main/ael-files/usr/share/dev/ael-dev.db'
 
-def packages_list(mode='hacker'):
+def packages_list(modes=['hyprland', 'i3wm', 'pip']):
 
     con = sql.connect(DB)
     cur = con.cursor()
 
-    q = f"""SELECT package FROM packages WHERE mode LIKE '%{mode}%'"""
+    q = f"""SELECT package, mode FROM packages"""
     cur.execute(q)
     records = cur.fetchall()
 
-    return [p[0] for p in records]
+    records = [(p[0], p[1].split()) for p in records]
+
+    packages = []
+    for p in records:
+        if set(modes).intersection(p[1]):
+            packages.append(p[0])
+
+    return packages
 
     con.close
 
 def package_info(package):
 
-    pkg_Si = execute(f"paru -Si {package}", capture_output=True).stdout.decode("UTF-8").split("\n")
     package_info = {}
+
+    pkg_Si = execute(f"paru -Si {package}", capture_output=True).stdout.decode("UTF-8").split("\n")
     for line in pkg_Si:
         ## Package name.
-        package_info["package"] = package
+        package_info["name"] = package
         ## Package repository.
         if line.startswith("Repository"):
             package_info["repository"] = line.split(":")[1].strip()
@@ -51,12 +63,6 @@ def package_info(package):
         pass
 
     pkg_Ql = execute(f"paru -Qlq {package}", capture_output=True).stdout.decode("UTF-8").split("\n")
-    # for line in pkg_Ql:
-        # print(line)
-        # if line.endswith(".md"):
-            # print(line)
-            # execute(f"glow {line}")
-
     package_Ql_bin = []
     package_Ql_desktop = []
     package_Ql_man = []
@@ -90,19 +96,84 @@ def package_info(package):
         else:
             package_info["has_docs"] = False
 
-    package_info["Ql_bin"] = package_Ql_bin
+    package_info["bin"] = package_Ql_bin
     package_info["Ql_desktop"] = package_Ql_desktop
     package_info["Ql_man"] = package_Ql_man
     package_info["Ql_info"] = package_Ql_info
 
+    ## (* modes *)
+    con = sql.connect(DB)
+    cur = con.cursor()
+    q = f"""SELECT mode FROM packages WHERE package == '{package}'"""
+    cur.execute(q)
+    modes = cur.fetchone()
+
+    package_info["modes"] = [mode for mode in modes]
+    con.close
+
     return package_info
+
+def generate_packagestoml(modes=['i3wm', 'hyprland', 'pip']):
+
+    pkgs = sorted(packages_list(modes=modes))
+
+    packagestoml = "/home/ghost/main/ael-files/usr/share/ael/packages.toml"
+
+    with open(packagestoml, mode='w') as file_out:
+        file_out.write(f"""\
+## ----------------------------------------------------------------------- INFO
+## [/usr/share/ael/packages.toml]
+## author        : fantomH @alterEGO Linux
+## created       : 2023-10-22 15:06:13 UTC
+## updated       : {datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z')}
+## description   : Packages list.
+
+""")
+
+        for pkg in pkgs:
+            pkg_info = package_info(pkg)
+            file_out.write(f"""\
+[{pkg_info.get('name')}]
+repository      = "{pkg_info.get('repository')}"
+url             = "{pkg_info.get('url')}"
+description     = "{pkg_info.get('description')}"
+modes           = {pkg_info.get('modes')}
+
+""")
+
+        file_out.write(f"""\
+# vim: foldmethod=marker
+## ------------------------------------------------------------- FIN ¯\_(ツ)_/¯
+""")
+
+def packages_diff():
+
+    PACKAGES = os.path.join('/', 'home', 'ghost', 'main', 'ael-files', 'usr', 'share', 'ael', 'packages.toml')
+
+    with open(PACKAGES, 'rb') as _input:
+        data = tomllib.load(_input)
+
+        from_PACKAGES = [x for x in data.keys()]
+    
+    locally = execute(f"paru -Qeq", capture_output=True).stdout.decode("UTF-8").split("\n")
+
+    print(set(from_PACKAGES).difference(locally))
 
 def main():
 
-    pkgs = packages_list(mode='hacker-hyprland')
+    ## packages_list(mode='hyprland')
+    # for p in packages_list(modes=['hyprland']):
+        # print(p)
 
-    for p in pkgs:
-        print(package_info(p))
+    ## package_info(package)
+    # for p in packages_list(modes=['hyprland']):
+        # print(package_info(p))
+
+    ## (* Generate packages.toml *)
+    # generate_packagestoml()
+
+    ## (* Packages diff *)
+    packages_diff()
 
 if __name__ == "__main__":
     main()
