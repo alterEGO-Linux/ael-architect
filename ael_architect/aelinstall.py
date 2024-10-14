3a6a3fe5bbea8c92337601e18cee101191b60ab6
Pascal Malouin <pascal.malouin@gmail.com>
2024-10-14 12:08:34 -0400

    Updated using lazygit...

--- ael_architect/aelinstall.py
+++ ael_architect/aelinstall.py
@@ -11,7 +11,7 @@
 
 from pathlib import Path
 
-from archinstall import Installer, ProfileConfiguration, profile_handler, User
+from archinstall import Installer, profile, profile_handler, User
 from archinstall.default_profiles.minimal import MinimalProfile
 from archinstall.lib.disk.device_model import FilesystemType
 from archinstall.lib.disk.encryption_menu import DiskEncryptionMenu
@@ -48,7 +48,7 @@ with Installer(
 
     # Optionally, install a profile of choice.
     # In this case, we install a minimal profile that is empty
-    profile_config = ProfileConfiguration(MinimalProfile())
+    profile_config = profile.ProfileConfiguration(MinimalProfile())
     profile_handler.install_profile_config(installation, profile_config)
 
     user = User('hacker1', 'password', True)
